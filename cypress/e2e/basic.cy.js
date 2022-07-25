7eda4da5b4031c79c6024934b0f4c70d19890cac
Tara Z. Manicsic <tzmanics@gmail.com>
2022-07-25 16:08:29 -0400

    adds maintenance testing

---
+++ cypress/e2e/basic.cy.js
@@ -0,0 +1,16 @@
+describe('empty spec', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  it('displays the resources text', () => {
+    cy.get('h2')
+    .contains('Here are some resources to help you on your Hugo + Netlify journey');
+  })
+  it('renders the image', () => {
+    cy.get('img')
+    .should('be.visible')
+    .and(($img) => {
+      expect($img[0].naturalWidth).to.be.greaterThan(0);
+    })
+  })
+})
